5274ada01b8572b719d9737bcd06ae09460bfa3b
Kunal Shinde <124497605+KUNALSHINDE1515@users.noreply.github.com>
2023-12-09 13:16:35 +0530

    Add files via upload

---
+++ JavaSript/1.js
@@ -0,0 +1,16 @@
+// console.log("WElcome to Kunal Bharat Shinde");
+// console.log("I love JavaScript");
+// console.log("From Apn a collage");
+
+fullName="Kunal Shinde";
+console.log(fullName);
+
+age=21;
+console.log(age);
+
+price=1250.22
+x=null;
+console.log(x);
+y=undefined;
+console.log(y);
+console.log(price);
